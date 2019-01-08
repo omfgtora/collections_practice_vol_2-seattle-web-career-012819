18515c25fbfd2788fae9a6064ac355ad76028f21
Ethan Roberts <ejrobe@gmail.com>
2019-01-08 17:21:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
-# your code goes here
+def begins_with_r(arr)
+  arr.all? { |item| item.start_with?("r") }
+end
+
+def contain_a(arr)
+  arr.select { |item| item.include?('a') }
+end
+
+def first_wa(arr)
+  arr.delete_if { |item| !(item.is_a? String) }
+    .find { |item| item.start_with?("wa") }
+end
+
+def remove_non_strings(arr)
+  arr.delete_if { |item| !(item.is_a? String) }
+end
+
+def count_elements(arr)
+  arr
+    .group_by { |item| item[:name] }
+    .map { |key, value| {name: key, count: value.length} }
+end
+
+def merge_data(keys, data)
+  # for each in data find a match in keys
+  data.each do |x|
+    # find match in keys
+    # insert data that matches with key
+  end
+end
+
+def find_cool(arr)
+  x = Array.new
+  for each in arr
+    if each["temperature"] == "cool"
+      x << each
+    else
+      nil
+    end
+  end
+  puts x
+end
+
+def organize_schools(arr)
+  arr.to_h
+  arr.sort_by {|k, v| v}
+end
